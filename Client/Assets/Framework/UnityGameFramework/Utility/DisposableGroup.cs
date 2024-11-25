c4a25e7d1422fc75b77e2728cccc8d0be4b101ac
SunHowe <tosunhowe@gmail.com>
2024-11-25 18:25:30 +0800

    [feat] DisposableGroup

---
+++ Client/Assets/Framework/UnityGameFramework/Utility/DisposableGroup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 可批量调用IDisposable.Dispose()的容器。
+    /// </summary>
+    public sealed class DisposableGroup : IDisposable
+    {
+        private readonly List<IDisposable> m_Disposables = new List<IDisposable>();
+
+        public void Add(IDisposable disposable)
+        {
+            if (disposable == null)
+            {
+                throw new ArgumentNullException(nameof(disposable));
+            }
+
+            m_Disposables.Add(disposable);
+        }
+
+        public void Dispose()
+        {
+            for (var index = m_Disposables.Count - 1; index >= 0; index--)
+            {
+                var disposable = m_Disposables[index];
+                try
+                {
+                    disposable.Dispose();
+                }
+                catch (Exception e)
+                {
+                    Log.Fatal(e.ToString());
+                }
+            }
+            
+            m_Disposables.Clear();
+        }
+    }
+}
