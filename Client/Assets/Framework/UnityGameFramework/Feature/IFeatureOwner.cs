4e09fcfac7cb9f7c277b8aa69253fc7a4c22d7a5
SunHowe <tosunhowe@gmail.com>
2024-11-25 18:31:13 +0800

    [feat] 子功能模块

---
+++ Client/Assets/Framework/UnityGameFramework/Feature/IFeatureOwner.cs
@@ -0,0 +1,10 @@
+﻿namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 功能持有者。功能持有者可以持有一个功能容器，可供各模块自行组织使用一些子功能组件。
+    /// </summary>
+    public interface IFeatureOwner
+    {
+        
+    }
+}
