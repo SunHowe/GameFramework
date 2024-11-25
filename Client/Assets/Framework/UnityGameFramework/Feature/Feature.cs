5450d30cc8016b5c4b43498ac4164ef9da3feca6
SunHowe <tosunhowe@gmail.com>
2024-11-25 22:08:33 +0800

    [feat] 子功能管理

---
+++ Client/Assets/Framework/UnityGameFramework/Feature/Feature.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 子功能接口。
+    /// </summary>
+    public interface IFeature
+    {
+        /// <summary>
+        /// 初始化子功能，由框架调度进行。
+        /// </summary>
+        void Awake(IFeatureOwner featureOwner);
+
+        /// <summary>
+        /// 销毁子功能，由框架调度进行。
+        /// </summary>
+        void Shutdown();
+    }
+    
+    /// <summary>
+    /// 子功能基类。
+    /// </summary>
+    public abstract class Feature : IFeature
+    {
+        /// <summary>
+        /// 功能持有者。
+        /// </summary>
+        public IFeatureOwner Owner { get; private set; }
+        
+        /// <summary>
+        /// 初始化子功能，由框架调度进行。
+        /// </summary>
+        public virtual void Awake(IFeatureOwner featureOwner)
+        {
+            Owner = featureOwner;
+        }
+
+        /// <summary>
+        /// 销毁子功能，由框架调度进行。
+        /// </summary>
+        public abstract void Shutdown();
+    }
+
+    /// <summary>
+    /// 子功能基类，对持有者有类型约束，若类型不匹配会直接触发异常。
+    /// </summary>
+    public abstract class Feature<T> : IFeature where T : IFeatureOwner
+    {
+        /// <summary>
+        /// 功能持有者。
+        /// </summary>
+        public T Owner { get; private set; }
+        
+        /// <summary>
+        /// 初始化子功能，由框架调度进行。
+        /// </summary>
+        public virtual void Awake(IFeatureOwner featureOwner)
+        {
+            if (!(featureOwner is T distFeatureOwner))
+            {
+                throw new ArgumentNullException(nameof(featureOwner));
+            }
+            
+            Owner = distFeatureOwner;
+        }
+
+        /// <summary>
+        /// 销毁子功能，由框架调度进行。
+        /// </summary>
+        public abstract void Shutdown();
+    }
+}
---
+++ Client/Assets/Framework/UnityGameFramework/Feature/FeatureContainer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 功能容器，用于统一管理子功能的生命周期。
+    /// </summary>
+    public class FeatureContainer
+    {
+        /// <summary>
+        /// 功能持有者。
+        /// </summary>
+        public IFeatureOwner Owner { get; }
+
+        private readonly Dictionary<Type, IFeature> m_FeatureDict = new Dictionary<Type, IFeature>();
+        private readonly List<IFeature> m_FeatureList = new List<IFeature>();
+
+        public FeatureContainer(IFeatureOwner owner)
+        {
+            Owner = owner;
+        }
+
+        /// <summary>
+        /// 销毁容器，并销毁所有子功能。
+        /// </summary>
+        public void Shutdown()
+        {
+            for (var index = m_FeatureList.Count - 1; index >= 0; index--)
+            {
+                var feature = m_FeatureList[index];
+                try
+                {
+                    feature.Shutdown();
+                }
+                catch (Exception e)
+                {
+                    Log.Fatal(e.ToString());
+                }
+            }
+
+            m_FeatureDict.Clear();
+            m_FeatureList.Clear();
+        }
+
+        /// <summary>
+        /// 添加子功能。
+        /// </summary>
+        public T AddFeature<T>() where T : class, IFeature, new()
+        {
+            var featureType = typeof(T);
+            if (m_FeatureDict.ContainsKey(featureType))
+            {
+                throw new Exception($"Feature {featureType} already added.");
+            }
+
+            var feature = new T();
+            feature.Awake(Owner);
+
+            m_FeatureDict.Add(featureType, feature);
+            m_FeatureList.Add(feature);
+
+            return feature;
+        }
+
+        /// <summary>
+        /// 获取指定类型的子功能。
+        /// </summary>
+        public T GetFeature<T>() where T : class, IFeature
+        {
+            return m_FeatureDict.TryGetValue(typeof(T), out var feature) ? (T)feature : null;
+        }
+
+        /// <summary>
+        /// 判断是否有指定的子功能
+        /// </summary>
+        public bool HasFeature<T>() where T : class, IFeature
+        {
+            return m_FeatureDict.ContainsKey(typeof(T));
+        }
+
+        /// <summary>
+        /// 移除指定子功能。
+        /// </summary>
+        public void RemoveFeature<T>() where T : class, IFeature
+        {
+            if (!m_FeatureDict.TryGetValue(typeof(T), out var feature))
+            {
+                return;
+            }
+
+            try
+            {
+                feature.Shutdown();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e.ToString());
+            }
+            
+            m_FeatureDict.Remove(typeof(T));
+            m_FeatureList.Remove(feature);
+        }
+    }
+}
