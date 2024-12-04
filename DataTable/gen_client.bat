76d4bceae93c1e5cf58d849b7e51c5d9f3764d52
SunHowe <tosunhowe@gmail.com>
2024-12-04 14:45:16 +0800

    [refactor] 客户端配置表代码生成使用customTemplateDir参数，这样直接用原生的Luban工具即可。

--- DataTable/gen_client.bat
+++ DataTable/gen_client.bat
@@ -1,27 +1,18 @@
 set WORKSPACE=..
 set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
+set LUBAN_TEMPLATE_DIR=%WORKSPACE%\Tools\LubanClientTemplate
 set CONF_ROOT=.
-
-set OUTPUT_DATA_DIR_SERVER=output_server
-set OUTPUT_CODE_DIR_SERVER=output_code_server
 
 set OUTPUT_DATA_DIR_CLIENT=%WORKSPACE%\Client\Assets\GameMain\DataTable
 set OUTPUT_CODE_DIR_CLIENT=%WORKSPACE%\Client\Assets\GameMain\Scripts\GameLogic\DataTable\AutoGen
 
 dotnet %LUBAN_DLL% ^
+    --customTemplateDir %LUBAN_TEMPLATE_DIR% ^
     -t client ^
-    -c cs-gf-bin ^
+    -c cs-bin ^
     -d bin ^
     --conf %CONF_ROOT%\luban.conf ^
     -x outputDataDir=%OUTPUT_DATA_DIR_CLIENT% ^
     -x outputCodeDir=%OUTPUT_CODE_DIR_CLIENT%
-
-@REM dotnet %LUBAN_DLL% ^
-@REM     -t server ^
-@REM     -c cs-bin ^
-@REM     -d bin ^
-@REM     --conf %CONF_ROOT%\luban.conf ^
-@REM     -x outputDataDir=%OUTPUT_DATA_DIR_SERVER% ^
-@REM     -x outputCodeDir=%OUTPUT_CODE_DIR_SERVER%
 
 pause
