dc41d7b0f2dc771f6baccd6215de4d1e7a713003
SunHowe <tosunhowe@gmail.com>
2024-12-05 18:18:01 +0800

    [chore] 逻辑错误码定义

---
+++ Client/Assets/GameMain/Scripts/GameLogic/GameErrorCode.cs
@@ -0,0 +1,12 @@
+﻿using UnityGameFramework.Runtime;
+
+namespace GameLogic
+{
+    /// <summary>
+    /// 游戏逻辑定义的错误码。
+    /// </summary>
+    public enum GameErrorCode
+    {
+        Min = FrameworkErrorCode.Max,
+    }
+}
