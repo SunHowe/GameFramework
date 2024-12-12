2de8537e17c062445eb0cfa3fca4fb5c640f7ade
SunHowe <tosunhowe@gmail.com>
2024-12-12 17:37:05 +0800

    [feat] 异步功能支持

---
+++ Client/Assets/Framework/UnityGameFramework/Feature/Async/AsyncFeature.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 异步功能。提供一些异步相关的支持。
+    /// </summary>
+    public sealed class AsyncFeature : Feature
+    {
+        /// <summary>
+        /// 取消令牌。当功能模块被销毁时，会被标记为取消，
+        /// </summary>
+        public CancellationToken CancellationToken => (m_CancellationTokenSource ??= new CancellationTokenSource()).Token;
+
+        private CancellationTokenSource m_CancellationTokenSource;
+
+        public override void Awake(object featureOwner)
+        {
+            base.Awake(featureOwner);
+            m_CancellationTokenSource = null;
+        }
+
+        public override void Shutdown()
+        {
+            m_CancellationTokenSource?.Cancel();
+        }
+    }
+}
---
+++ Client/Assets/Framework/UnityGameFramework/Feature/Async/AsyncFeatureExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 异步请求功能拓展函数。
+    /// </summary>
+    public static class AsyncFeatureExtensions
+    {
+        /// <summary>
+        /// 获取异步取消令牌。
+        /// </summary>
+        public static CancellationToken GetCancellationToken(this FeatureContainer container)
+        {
+            return container.AddFeature<AsyncFeature>().CancellationToken;
+        }
+        
+        /// <summary>
+        /// 获取异步取消令牌。
+        /// </summary>
+        public static CancellationToken GetCancellationToken(this IFeatureContainerOwner owner)
+        {
+            return owner.FeatureContainer.AddFeature<AsyncFeature>().CancellationToken;
+        }
+    }
+}
