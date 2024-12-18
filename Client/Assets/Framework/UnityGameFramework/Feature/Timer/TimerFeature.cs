2e3b3f866a49fdb59ea3d05d421eb85ae088dc69
SunHowe <tosunhowe@gmail.com>
2024-12-18 09:07:29 +0800

    [feat] 定时器功能模块功能支持。

---
+++ Client/Assets/Framework/UnityGameFramework/Feature/Timer/TimerFeature.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using GameFramework.Timer;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 定时器功能模块。用于管理由自己创建的定时器，并提供帧更新接口注册的支持。
+    /// </summary>
+    public sealed class TimerFeature : IFeature
+    {
+        private readonly HashSet<int> m_TimerIdSet = new HashSet<int>();
+
+        /// <summary>
+        /// 添加无限重复的定时器。
+        /// </summary>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public int AddTimer(float interval, TimerCallback callback)
+        {
+            var id = TimerComponent.Instance.AddTimer(interval, callback);
+            m_TimerIdSet.Add(id);
+            return id;
+        }
+
+        /// <summary>
+        /// 添加无限重复的定时器。
+        /// </summary>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <param name="userData">用户数据。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public int AddTimer(float interval, TimerCallback callback, object userData)
+        {
+            var id = TimerComponent.Instance.AddTimer(interval, callback, userData);
+            m_TimerIdSet.Add(id);
+            return id;
+        }
+
+        /// <summary>
+        /// 添加重复指定次数的定时器。
+        /// </summary>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="repeatTimes">重复次数。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public int AddTimer(float interval, int repeatTimes, TimerCallback callback)
+        {
+            var id = TimerComponent.Instance.AddTimer(interval, repeatTimes, callback);
+            m_TimerIdSet.Add(id);
+            return id;
+        }
+
+        /// <summary>
+        /// 添加重复指定次数的定时器。
+        /// </summary>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="repeatTimes">重复次数。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <param name="userData">用户数据。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public int AddTimer(float interval, int repeatTimes, TimerCallback callback, object userData)
+        {
+            var id = TimerComponent.Instance.AddTimer(interval, repeatTimes, callback, userData);
+            m_TimerIdSet.Add(id);
+            return id;
+        }
+
+        /// <summary>
+        /// 通过定时器id移除定时器。
+        /// </summary>
+        /// <param name="timerId">定时器id。</param>
+        public void RemoveTimer(int timerId)
+        {
+            if (!m_TimerIdSet.Remove(timerId))
+            {
+                return;
+            }
+
+            TimerComponent.Instance.RemoveTimer(timerId);
+        }
+
+        public void Awake(object featureOwner)
+        {
+        }
+
+        public void Shutdown()
+        {
+            foreach (var id in m_TimerIdSet)
+            {
+                TimerComponent.Instance.RemoveTimer(id);
+            }
+            
+            m_TimerIdSet.Clear();
+        }
+    }
+}
---
+++ Client/Assets/Framework/UnityGameFramework/Feature/Timer/TimerFeatureExtensions.cs
@@ -0,0 +1,134 @@
+﻿using GameFramework.Timer;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 定时器功能模块拓展方法。
+    /// </summary>
+    public static class TimerFeatureExtensions
+    {
+        /// <summary>
+        /// 添加无限重复的定时器。
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public static int AddTimer(FeatureContainer container, float interval, TimerCallback callback)
+        {
+            return container.AddFeature<TimerFeature>().AddTimer(interval, callback);
+        }
+
+        /// <summary>
+        /// 添加无限重复的定时器。
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <param name="userData">用户数据。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public static int AddTimer(FeatureContainer container, float interval, TimerCallback callback, object userData)
+        {
+            return container.AddFeature<TimerFeature>().AddTimer(interval, callback, userData);
+        }
+
+        /// <summary>
+        /// 添加重复指定次数的定时器。
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="repeatTimes">重复次数。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public static int AddTimer(FeatureContainer container, float interval, int repeatTimes, TimerCallback callback)
+        {
+            return container.AddFeature<TimerFeature>().AddTimer(interval, repeatTimes, callback);
+        }
+
+        /// <summary>
+        /// 添加重复指定次数的定时器。
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="repeatTimes">重复次数。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <param name="userData">用户数据。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public static int AddTimer(FeatureContainer container, float interval, int repeatTimes, TimerCallback callback, object userData)
+        {
+            return container.AddFeature<TimerFeature>().AddTimer(interval, repeatTimes, callback, userData);
+        }
+
+        /// <summary>
+        /// 通过定时器id移除定时器。
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="timerId">定时器id。</param>
+        public static void RemoveTimer(FeatureContainer container, int timerId)
+        {
+            container.AddFeature<TimerFeature>().RemoveTimer(timerId);
+        }
+
+        /// <summary>
+        /// 添加无限重复的定时器。
+        /// </summary>
+        /// <param name="owner"></param>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public static int AddTimer(IFeatureContainerOwner owner, float interval, TimerCallback callback)
+        {
+            return owner.FeatureContainer.AddFeature<TimerFeature>().AddTimer(interval, callback);
+        }
+
+        /// <summary>
+        /// 添加无限重复的定时器。
+        /// </summary>
+        /// <param name="owner"></param>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <param name="userData">用户数据。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public static int AddTimer(IFeatureContainerOwner owner, float interval, TimerCallback callback, object userData)
+        {
+            return owner.FeatureContainer.AddFeature<TimerFeature>().AddTimer(interval, callback, userData);
+        }
+
+        /// <summary>
+        /// 添加重复指定次数的定时器。
+        /// </summary>
+        /// <param name="owner"></param>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="repeatTimes">重复次数。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public static int AddTimer(IFeatureContainerOwner owner, float interval, int repeatTimes, TimerCallback callback)
+        {
+            return owner.FeatureContainer.AddFeature<TimerFeature>().AddTimer(interval, repeatTimes, callback);
+        }
+
+        /// <summary>
+        /// 添加重复指定次数的定时器。
+        /// </summary>
+        /// <param name="owner"></param>
+        /// <param name="interval">间隔时间。</param>
+        /// <param name="repeatTimes">重复次数。</param>
+        /// <param name="callback">定时器回调。</param>
+        /// <param name="userData">用户数据。</param>
+        /// <returns>定时器id 用于手动停止定时器。</returns>
+        public static int AddTimer(IFeatureContainerOwner owner, float interval, int repeatTimes, TimerCallback callback, object userData)
+        {
+            return owner.FeatureContainer.AddFeature<TimerFeature>().AddTimer(interval, repeatTimes, callback, userData);
+        }
+
+        /// <summary>
+        /// 通过定时器id移除定时器。
+        /// </summary>
+        /// <param name="owner"></param>
+        /// <param name="timerId">定时器id。</param>
+        public static void RemoveTimer(IFeatureContainerOwner owner, int timerId)
+        {
+            owner.FeatureContainer.AddFeature<TimerFeature>().RemoveTimer(timerId);
+        }
+    }
+}
