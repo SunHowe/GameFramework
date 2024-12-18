362e6b288c07b8e9402a776baecef604893702bc
SunHowe <tosunhowe@gmail.com>
2024-12-18 13:14:05 +0800

    [feat] 本地化文本配置生成

--- DataTable/gen_client_localization.bat
+++ DataTable/gen_client_localization.bat
@@ -3,15 +3,19 @@ set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
 set LUBAN_TEMPLATE_DIR=%WORKSPACE%\Tools\LubanClientTemplate
 set CONF_ROOT=.
 
-set OUTPUT_DATA_DIR_LAUNCHER=%WORKSPACE%\Client\Assets\GameLauncher\DataTable
-set OUTPUT_CODE_DIR_LAUNCHER=%WORKSPACE%\Client\Assets\GameMain\Scripts\GameMono\DataTable\AutoGen
+set OUTPUT_DATA_DIR_LAUNCHER=%WORKSPACE%\Client\Assets\GameLauncher\Localization
+set OUTPUT_DATA_DIR_MAIN=%WORKSPACE%\Client\Assets\GameMain\Localization
 
 dotnet %LUBAN_DLL% ^
-    --customTemplateDir %LUBAN_TEMPLATE_DIR% ^
-    -t launcher ^
+    -t client ^
+    -d bin ^
+    --conf %CONF_ROOT%\luban_localization_launch.conf ^
+    -x outputDataDir=%OUTPUT_DATA_DIR_LAUNCHER%
+
+dotnet %LUBAN_DLL% ^
+    -t client ^
     -d bin ^
     --conf %CONF_ROOT%\luban_localization.conf ^
-    -x outputDataDir=%OUTPUT_DATA_DIR_LAUNCHER% ^
-    -x outputCodeDir=%OUTPUT_CODE_DIR_LAUNCHER%
+    -x outputDataDir=%OUTPUT_DATA_DIR_MAIN%
 
 pause
