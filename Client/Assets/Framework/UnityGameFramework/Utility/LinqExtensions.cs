fcf6c0d48f9b37b82c92c86969275660172013a0
SunHowe <tosunhowe@gmail.com>
2024-12-20 15:36:26 +0800

    [feat] Linq拓展

---
+++ Client/Assets/Framework/UnityGameFramework/Utility/LinqExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// Linq拓展方法。
+    /// </summary>
+    public static class LinqExtensions
+    {
+        /// <summary>
+        /// 将自己添加到指定的List容器。
+        /// </summary>
+        public static T AddTo<T>(this T item, List<T> buffer)
+        {
+            buffer.Add(item);
+            return item;
+        }
+
+        /// <summary>
+        /// 将自己添加到指定的IList容器。
+        /// </summary>
+        public static T AddTo<T>(this T item, IList<T> buffer)
+        {
+            buffer.Add(item);
+            return item;
+        }
+
+        /// <summary>
+        /// 将自己添加到指定的ICollection容器。
+        /// </summary>
+        public static T AddTo<T>(this T item, ICollection<T> buffer)
+        {
+            buffer.Add(item);
+            return item;
+        }
+    }
+}
