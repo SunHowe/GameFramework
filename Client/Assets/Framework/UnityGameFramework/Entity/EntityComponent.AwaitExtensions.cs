59c835b4c4c052d75371b670dbcae6efaf28ea8c
SunHowe <tosunhowe@gmail.com>
2024-12-20 17:25:46 +0800

    [feat] EntityComponent ShowEntityAsync对取消令牌的支持

--- Client/Assets/Framework/UnityGameFramework/Entity/EntityComponent.AwaitExtensions.cs
+++ Client/Assets/Framework/UnityGameFramework/Entity/EntityComponent.AwaitExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 
 namespace UnityGameFramework.Runtime
@@ -6,120 +8,125 @@ namespace UnityGameFramework.Runtime
     public partial class EntityComponent
     {
         /// <summary>
-        /// 使用自增长的实体id显示实体。
+        /// 记录正在Await中的实体id集合。
         /// </summary>
-        /// <typeparam name="T">实体逻辑类型。</typeparam>
-        /// <param name="entityAssetName">实体资源名称。</param>
-        /// <param name="entityGroupName">实体组名称。</param>
-        public UniTask<Entity> ShowEntityAsync<T>(string entityAssetName, string entityGroupName) where T : EntityLogic
-        {
-            return ShowEntityAsync(typeof(T), entityAssetName, entityGroupName, DefaultPriority, null);
-        }
-
-        /// <summary>
-        /// 使用自增长的实体id显示实体。
-        /// </summary>
-        /// <param name="entityLogicType">实体逻辑类型。</param>
-        /// <param name="entityAssetName">实体资源名称。</param>
-        /// <param name="entityGroupName">实体组名称。</param>
-        public UniTask<Entity> ShowEntityAsync(Type entityLogicType, string entityAssetName, string entityGroupName)
-        {
-            return ShowEntityAsync(entityLogicType, entityAssetName, entityGroupName, DefaultPriority, null);
-        }
-
-        /// <summary>
-        /// 使用自增长的实体id显示实体。
-        /// </summary>
-        /// <typeparam name="T">实体逻辑类型。</typeparam>
-        /// <param name="entityAssetName">实体资源名称。</param>
-        /// <param name="entityGroupName">实体组名称。</param>
-        /// <param name="priority">加载实体资源的优先级。</param>
-        public UniTask<Entity> ShowEntityAsync<T>(string entityAssetName, string entityGroupName, int priority) where T : EntityLogic
-        {
-            return ShowEntityAsync(typeof(T), entityAssetName, entityGroupName, priority, null);
-        }
-
-        /// <summary>
-        /// 使用自增长的实体id显示实体。
-        /// </summary>
-        /// <param name="entityLogicType">实体逻辑类型。</param>
-        /// <param name="entityAssetName">实体资源名称。</param>
-        /// <param name="entityGroupName">实体组名称。</param>
-        /// <param name="priority">加载实体资源的优先级。</param>
-        public UniTask<Entity> ShowEntityAsync(Type entityLogicType, string entityAssetName, string entityGroupName, int priority)
-        {
-            return ShowEntityAsync(entityLogicType, entityAssetName, entityGroupName, priority, null);
-        }
-
-        /// <summary>
-        /// 使用自增长的实体id显示实体。
-        /// </summary>
-        /// <typeparam name="T">实体逻辑类型。</typeparam>
-        /// <param name="entityAssetName">实体资源名称。</param>
-        /// <param name="entityGroupName">实体组名称。</param>
-        /// <param name="userData">用户自定义数据。</param>
-        public UniTask<Entity> ShowEntityAsync<T>(string entityAssetName, string entityGroupName, object userData) where T : EntityLogic
-        {
-            return ShowEntityAsync(typeof(T), entityAssetName, entityGroupName, DefaultPriority, userData);
-        }
-
-        /// <summary>
-        /// 使用自增长的实体id显示实体。
-        /// </summary>
-        /// <param name="entityLogicType">实体逻辑类型。</param>
-        /// <param name="entityAssetName">实体资源名称。</param>
-        /// <param name="entityGroupName">实体组名称。</param>
-        /// <param name="userData">用户自定义数据。</param>
-        public UniTask<Entity> ShowEntityAsync(Type entityLogicType, string entityAssetName, string entityGroupName, object userData)
-        {
-            return ShowEntityAsync(entityLogicType, entityAssetName, entityGroupName, DefaultPriority, userData);
-        }
-
-        /// <summary>
-        /// 使用自增长的实体id显示实体。
-        /// </summary>
-        /// <typeparam name="T">实体逻辑类型。</typeparam>
-        /// <param name="entityAssetName">实体资源名称。</param>
-        /// <param name="entityGroupName">实体组名称。</param>
-        /// <param name="priority">加载实体资源的优先级。</param>
-        /// <param name="userData">用户自定义数据。</param>
-        public UniTask<Entity> ShowEntityAsync<T>(string entityAssetName, string entityGroupName, int priority, object userData) where T : EntityLogic
-        {
-            return ShowEntityAsync(typeof(T), entityAssetName, entityGroupName, priority, userData);
-        }
-
-        /// <summary>
-        /// 使用自增长的实体id显示实体。
-        /// </summary>
-        /// <param name="entityLogicType">实体逻辑类型。</param>
-        /// <param name="entityAssetName">实体资源名称。</param>
-        /// <param name="entityGroupName">实体组名称。</param>
-        /// <param name="priority">加载实体资源的优先级。</param>
-        /// <param name="userData">用户自定义数据。</param>
-        public UniTask<Entity> ShowEntityAsync(Type entityLogicType, string entityAssetName, string entityGroupName, int priority, object userData)
-        {
-            if (entityLogicType == null)
-            {
-                Log.Error("Entity type is invalid.");
-                return UniTask.FromResult<Entity>(null);
-            }
-
-            var task = AutoResetUniTaskCompletionSource<Entity>.Create();
-            var entityId = SpawnEntityId();
-            m_EntityManager.ShowEntity(entityId, entityAssetName, entityGroupName, priority, ShowEntityInfo.Create(entityLogicType, userData, task));
-            return task.Task;
-        }
+        private readonly HashSet<int> m_AwaitLoadingEntityIdSet = new HashSet<int>();
         
         /// <summary>
+        /// 使用自增长的实体id显示实体。
+        /// </summary>
+        /// <typeparam name="T">实体逻辑类型。</typeparam>
+        /// <param name="entityAssetName">实体资源名称。</param>
+        /// <param name="entityGroupName">实体组名称。</param>
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync<T>(string entityAssetName, string entityGroupName, CancellationToken token) where T : EntityLogic
+        {
+            return ShowEntityAsync(typeof(T), entityAssetName, entityGroupName, DefaultPriority, null, token);
+        }
+
+        /// <summary>
+        /// 使用自增长的实体id显示实体。
+        /// </summary>
+        /// <param name="entityLogicType">实体逻辑类型。</param>
+        /// <param name="entityAssetName">实体资源名称。</param>
+        /// <param name="entityGroupName">实体组名称。</param>
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync(Type entityLogicType, string entityAssetName, string entityGroupName, CancellationToken token)
+        {
+            return ShowEntityAsync(entityLogicType, entityAssetName, entityGroupName, DefaultPriority, null, token);
+        }
+
+        /// <summary>
+        /// 使用自增长的实体id显示实体。
+        /// </summary>
+        /// <typeparam name="T">实体逻辑类型。</typeparam>
+        /// <param name="entityAssetName">实体资源名称。</param>
+        /// <param name="entityGroupName">实体组名称。</param>
+        /// <param name="priority">加载实体资源的优先级。</param>
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync<T>(string entityAssetName, string entityGroupName, int priority, CancellationToken token) where T : EntityLogic
+        {
+            return ShowEntityAsync(typeof(T), entityAssetName, entityGroupName, priority, null, token);
+        }
+
+        /// <summary>
+        /// 使用自增长的实体id显示实体。
+        /// </summary>
+        /// <param name="entityLogicType">实体逻辑类型。</param>
+        /// <param name="entityAssetName">实体资源名称。</param>
+        /// <param name="entityGroupName">实体组名称。</param>
+        /// <param name="priority">加载实体资源的优先级。</param>
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync(Type entityLogicType, string entityAssetName, string entityGroupName, int priority, CancellationToken token)
+        {
+            return ShowEntityAsync(entityLogicType, entityAssetName, entityGroupName, priority, null, token);
+        }
+
+        /// <summary>
+        /// 使用自增长的实体id显示实体。
+        /// </summary>
+        /// <typeparam name="T">实体逻辑类型。</typeparam>
+        /// <param name="entityAssetName">实体资源名称。</param>
+        /// <param name="entityGroupName">实体组名称。</param>
+        /// <param name="userData">用户自定义数据。</param>
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync<T>(string entityAssetName, string entityGroupName, object userData, CancellationToken token) where T : EntityLogic
+        {
+            return ShowEntityAsync(typeof(T), entityAssetName, entityGroupName, DefaultPriority, userData, token);
+        }
+
+        /// <summary>
+        /// 使用自增长的实体id显示实体。
+        /// </summary>
+        /// <param name="entityLogicType">实体逻辑类型。</param>
+        /// <param name="entityAssetName">实体资源名称。</param>
+        /// <param name="entityGroupName">实体组名称。</param>
+        /// <param name="userData">用户自定义数据。</param>
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync(Type entityLogicType, string entityAssetName, string entityGroupName, object userData, CancellationToken token)
+        {
+            return ShowEntityAsync(entityLogicType, entityAssetName, entityGroupName, DefaultPriority, userData, token);
+        }
+
+        /// <summary>
+        /// 使用自增长的实体id显示实体。
+        /// </summary>
+        /// <typeparam name="T">实体逻辑类型。</typeparam>
+        /// <param name="entityAssetName">实体资源名称。</param>
+        /// <param name="entityGroupName">实体组名称。</param>
+        /// <param name="priority">加载实体资源的优先级。</param>
+        /// <param name="userData">用户自定义数据。</param>
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync<T>(string entityAssetName, string entityGroupName, int priority, object userData, CancellationToken token) where T : EntityLogic
+        {
+            return ShowEntityAsync(typeof(T), entityAssetName, entityGroupName, priority, userData, token);
+        }
+
+        /// <summary>
+        /// 使用自增长的实体id显示实体。
+        /// </summary>
+        /// <param name="entityLogicType">实体逻辑类型。</param>
+        /// <param name="entityAssetName">实体资源名称。</param>
+        /// <param name="entityGroupName">实体组名称。</param>
+        /// <param name="priority">加载实体资源的优先级。</param>
+        /// <param name="userData">用户自定义数据。</param>
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync(Type entityLogicType, string entityAssetName, string entityGroupName, int priority, object userData, CancellationToken token)
+        {
+            return ShowEntityAsync(SpawnEntityId(), entityLogicType, entityAssetName, entityGroupName, priority, userData, token);
+        }
+
+        /// <summary>
         /// 显示实体。
         /// </summary>
         /// <typeparam name="T">实体逻辑类型。</typeparam>
         /// <param name="entityId">实体编号。</param>
         /// <param name="entityAssetName">实体资源名称。</param>
         /// <param name="entityGroupName">实体组名称。</param>
-        public UniTask<Entity> ShowEntityAsync<T>(int entityId, string entityAssetName, string entityGroupName) where T : EntityLogic
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync<T>(int entityId, string entityAssetName, string entityGroupName, CancellationToken token) where T : EntityLogic
         {
-            return ShowEntityAsync(entityId, typeof(T), entityAssetName, entityGroupName, DefaultPriority, null);
+            return ShowEntityAsync(entityId, typeof(T), entityAssetName, entityGroupName, DefaultPriority, null, token);
         }
 
         /// <summary>
@@ -129,9 +136,10 @@ namespace UnityGameFramework.Runtime
         /// <param name="entityLogicType">实体逻辑类型。</param>
         /// <param name="entityAssetName">实体资源名称。</param>
         /// <param name="entityGroupName">实体组名称。</param>
-        public UniTask<Entity> ShowEntityAsync(int entityId, Type entityLogicType, string entityAssetName, string entityGroupName)
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync(int entityId, Type entityLogicType, string entityAssetName, string entityGroupName, CancellationToken token)
         {
-            return ShowEntityAsync(entityId, entityLogicType, entityAssetName, entityGroupName, DefaultPriority, null);
+            return ShowEntityAsync(entityId, entityLogicType, entityAssetName, entityGroupName, DefaultPriority, null, token);
         }
 
         /// <summary>
@@ -142,9 +150,10 @@ namespace UnityGameFramework.Runtime
         /// <param name="entityAssetName">实体资源名称。</param>
         /// <param name="entityGroupName">实体组名称。</param>
         /// <param name="priority">加载实体资源的优先级。</param>
-        public UniTask<Entity> ShowEntityAsync<T>(int entityId, string entityAssetName, string entityGroupName, int priority) where T : EntityLogic
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync<T>(int entityId, string entityAssetName, string entityGroupName, int priority, CancellationToken token) where T : EntityLogic
         {
-            return ShowEntityAsync(entityId, typeof(T), entityAssetName, entityGroupName, priority, null);
+            return ShowEntityAsync(entityId, typeof(T), entityAssetName, entityGroupName, priority, null, token);
         }
 
         /// <summary>
@@ -155,9 +164,10 @@ namespace UnityGameFramework.Runtime
         /// <param name="entityAssetName">实体资源名称。</param>
         /// <param name="entityGroupName">实体组名称。</param>
         /// <param name="priority">加载实体资源的优先级。</param>
-        public UniTask<Entity> ShowEntityAsync(int entityId, Type entityLogicType, string entityAssetName, string entityGroupName, int priority)
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync(int entityId, Type entityLogicType, string entityAssetName, string entityGroupName, int priority, CancellationToken token)
         {
-            return ShowEntityAsync(entityId, entityLogicType, entityAssetName, entityGroupName, priority, null);
+            return ShowEntityAsync(entityId, entityLogicType, entityAssetName, entityGroupName, priority, null, token);
         }
 
         /// <summary>
@@ -168,9 +178,10 @@ namespace UnityGameFramework.Runtime
         /// <param name="entityAssetName">实体资源名称。</param>
         /// <param name="entityGroupName">实体组名称。</param>
         /// <param name="userData">用户自定义数据。</param>
-        public UniTask<Entity> ShowEntityAsync<T>(int entityId, string entityAssetName, string entityGroupName, object userData) where T : EntityLogic
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync<T>(int entityId, string entityAssetName, string entityGroupName, object userData, CancellationToken token) where T : EntityLogic
         {
-            return ShowEntityAsync(entityId, typeof(T), entityAssetName, entityGroupName, DefaultPriority, userData);
+            return ShowEntityAsync(entityId, typeof(T), entityAssetName, entityGroupName, DefaultPriority, userData, token);
         }
 
         /// <summary>
@@ -181,9 +192,10 @@ namespace UnityGameFramework.Runtime
         /// <param name="entityAssetName">实体资源名称。</param>
         /// <param name="entityGroupName">实体组名称。</param>
         /// <param name="userData">用户自定义数据。</param>
-        public UniTask<Entity> ShowEntityAsync(int entityId, Type entityLogicType, string entityAssetName, string entityGroupName, object userData)
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync(int entityId, Type entityLogicType, string entityAssetName, string entityGroupName, object userData, CancellationToken token)
         {
-            return ShowEntityAsync(entityId, entityLogicType, entityAssetName, entityGroupName, DefaultPriority, userData);
+            return ShowEntityAsync(entityId, entityLogicType, entityAssetName, entityGroupName, DefaultPriority, userData, token);
         }
 
         /// <summary>
@@ -195,9 +207,10 @@ namespace UnityGameFramework.Runtime
         /// <param name="entityGroupName">实体组名称。</param>
         /// <param name="priority">加载实体资源的优先级。</param>
         /// <param name="userData">用户自定义数据。</param>
-        public UniTask<Entity> ShowEntityAsync<T>(int entityId, string entityAssetName, string entityGroupName, int priority, object userData) where T : EntityLogic
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync<T>(int entityId, string entityAssetName, string entityGroupName, int priority, object userData, CancellationToken token) where T : EntityLogic
         {
-            return ShowEntityAsync(entityId, typeof(T), entityAssetName, entityGroupName, priority, userData);
+            return ShowEntityAsync(entityId, typeof(T), entityAssetName, entityGroupName, priority, userData, token);
         }
 
         /// <summary>
@@ -209,7 +222,8 @@ namespace UnityGameFramework.Runtime
         /// <param name="entityGroupName">实体组名称。</param>
         /// <param name="priority">加载实体资源的优先级。</param>
         /// <param name="userData">用户自定义数据。</param>
-        public UniTask<Entity> ShowEntityAsync(int entityId, Type entityLogicType, string entityAssetName, string entityGroupName, int priority, object userData)
+        /// <param name="token">取消令牌票据。</param>
+        public UniTask<Entity> ShowEntityAsync(int entityId, Type entityLogicType, string entityAssetName, string entityGroupName, int priority, object userData, CancellationToken token)
         {
             if (entityLogicType == null)
             {
@@ -217,9 +231,29 @@ namespace UnityGameFramework.Runtime
                 return UniTask.FromResult<Entity>(null);
             }
 
+            if (token.IsCancellationRequested)
+            {
+                // 已经取消加载。
+                return UniTask.FromResult<Entity>(null);
+            }
+            
+            token.Register(OnCancellationTokenCancel, entityId);
+            m_AwaitLoadingEntityIdSet.Add(entityId);
+
             var task = AutoResetUniTaskCompletionSource<Entity>.Create();
             m_EntityManager.ShowEntity(entityId, entityAssetName, entityGroupName, priority, ShowEntityInfo.Create(entityLogicType, userData, task));
             return task.Task;
+        }
+
+        /// <summary>
+        /// 取消令牌标记为取消。
+        /// </summary>
+        private void OnCancellationTokenCancel(object obj)
+        {
+            if (obj is int entityId && m_AwaitLoadingEntityIdSet.Remove(entityId))
+            {
+                m_EntityManager.HideEntity(entityId);
+            }
         }
 
         /// <summary>
@@ -230,6 +264,7 @@ namespace UnityGameFramework.Runtime
             var showEntityInfo = (ShowEntityInfo)eventArgs.UserData;
             if (showEntityInfo.Task != null)
             {
+                m_AwaitLoadingEntityIdSet.Remove(eventArgs.Entity.Id);
                 showEntityInfo.Task.TrySetResult((Entity)eventArgs.Entity);
             }
         }
@@ -242,6 +277,7 @@ namespace UnityGameFramework.Runtime
             var showEntityInfo = (ShowEntityInfo)eventArgs.UserData;
             if (showEntityInfo.Task != null)
             {
+                m_AwaitLoadingEntityIdSet.Remove(eventArgs.EntityId);
                 showEntityInfo.Task.TrySetResult(null);
             }
         }
