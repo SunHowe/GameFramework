469490bd5a8912ffd5141d22b7826bd24b875432
SunHowe <tosunhowe@gmail.com>
2024-12-24 09:06:36 +0800

    [feat] DisposableObjectBase

---
+++ Client/Assets/Framework/UnityGameFramework/Utility/DisposableObjectBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GameFramework;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 可使用Dispose销毁的对象实例抽象类。
+    /// </summary>
+    public abstract class DisposableObjectBase : IReference, IDisposable
+    {
+        public abstract void Clear();
+
+        public void Dispose()
+        {
+            ReferencePool.Release(this);
+        }
+
+        public static T Create<T>() where T: DisposableObjectBase, new()
+        {
+            return ReferencePool.Acquire<T>();
+        }
+    }
+}
