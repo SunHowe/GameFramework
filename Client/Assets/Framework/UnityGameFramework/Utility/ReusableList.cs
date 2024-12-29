700173e7c87645b7a8b0dbec6df65ff9ca5e16d6
SunHowe <tosunhowe@gmail.com>
2024-12-29 15:30:06 +0800

    [feat] ReusableList

---
+++ Client/Assets/Framework/UnityGameFramework/Utility/ReusableList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using GameFramework;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 通过引用池维护的可复用列表。
+    /// </summary>
+    public sealed class ReusableList<T> : List<T>, IDisposable, IReference
+    {
+        /// <summary>
+        /// 销毁实例。
+        /// </summary>
+        public void Dispose()
+        {
+            ReferencePool.Release(this);
+        }
+
+        /// <summary>
+        /// 构造实例。
+        /// </summary>
+        public static ReusableList<T> Create()
+        {
+            return ReferencePool.Acquire<ReusableList<T>>();
+        }
+    }
+}
