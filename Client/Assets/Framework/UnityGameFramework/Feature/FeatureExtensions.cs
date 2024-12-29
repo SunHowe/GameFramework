707fe4a375d887fded4afa66fa96b58d0c18a6aa
SunHowe <tosunhowe@gmail.com>
2024-12-29 15:35:02 +0800

    [feat] FeatureOwnerObject
    [feat] GetFeatureOwner

---
+++ Client/Assets/Framework/UnityGameFramework/Feature/FeatureExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 功能模块拓展方法。
+    /// </summary>
+    public static class FeatureExtensions
+    {
+        /// <summary>
+        /// 获取GameObject的FeatureOwner实例。
+        /// </summary>
+        public static IFeatureContainerOwner GetFeatureOwner(this GameObject gameObject)
+        {
+            return gameObject.GetOrAddComponent<FeatureOwnerObject>();
+        }
+    }
+}
---
+++ Client/Assets/Framework/UnityGameFramework/Feature/FeatureOwnerObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 功能容器拥有者实例。用于添加到某个GameObject上，让其拥有管理功能的能力。
+    /// </summary>
+    public sealed class FeatureOwnerObject : MonoBehaviour, IFeatureContainerOwner
+    {
+        public FeatureContainer FeatureContainer { get; private set; }
+
+        private void Awake()
+        {
+            FeatureContainer = new FeatureContainer(this);
+        }
+
+        private void OnDestroy()
+        {
+            FeatureContainer.Shutdown();
+            FeatureContainer = null;
+        }
+    }
+}
