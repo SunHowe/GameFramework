06069069ad7585c6bff5da8d00a9f09580ebefd5
SunHowe <tosunhowe@gmail.com>
2024-12-29 23:17:25 +0800

    [feat] FeatureExtensions

--- Client/Assets/Framework/UnityGameFramework/Feature/FeatureExtensions.cs
+++ Client/Assets/Framework/UnityGameFramework/Feature/FeatureExtensions.cs
@@ -14,5 +14,37 @@ namespace UnityGameFramework.Runtime
         {
             return gameObject.GetOrAddComponent<FeatureOwnerObject>();
         }
+
+        /// <summary>
+        /// 添加子功能。
+        /// </summary>
+        public static T AddFeature<T>(this IFeatureContainerOwner owner) where T : class, IFeature, new()
+        {
+            return owner.FeatureContainer.AddFeature<T>();
+        }
+
+        /// <summary>
+        /// 获取指定类型的子功能。
+        /// </summary>
+        public static T GetFeature<T>(this IFeatureContainerOwner owner) where T : class, IFeature
+        {
+            return owner.FeatureContainer.GetFeature<T>();
+        }
+
+        /// <summary>
+        /// 判断是否有指定的子功能
+        /// </summary>
+        public static bool HasFeature<T>(this IFeatureContainerOwner owner) where T : class, IFeature
+        {
+            return owner.FeatureContainer.HasFeature<T>();
+        }
+
+        /// <summary>
+        /// 移除指定子功能。
+        /// </summary>
+        public static void RemoveFeature<T>(this IFeatureContainerOwner owner) where T : class, IFeature
+        {
+            owner.FeatureContainer.RemoveFeature<T>();
+        }
     }
 }
